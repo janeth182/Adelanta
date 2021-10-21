8e2545fc7d927388f02fdaa8c977718c62bd8401
jmariods <91164761+jvargas182@users.noreply.github.com>
2021-10-21 11:26:20 -0500

    Config extranet

--- Adelanta.Web/ClientApp/src/components/layout/content.js
+++ Adelanta.Web/ClientApp/src/components/layout/content.js
@@ -25,16 +25,7 @@ export const ContentComponent = ({ children }) => {
 						paddingBottom: 60,
 					}}
 				>
-					{/* <Breadcrumb style={{ margin: "16px 0" }}>
-						<Breadcrumb.Item>User</Breadcrumb.Item>
-						<Breadcrumb.Item>Bill</Breadcrumb.Item>
-					</Breadcrumb> */}
-					{/* <div
-						className="site-layout-background"
-						style={{ padding: 24, minHeight: 360 }}
-					> */}
 					{children}
-					{/* </div> */}
 				</Content>
 			</Layout>
 		</Layout>
